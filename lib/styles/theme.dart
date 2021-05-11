a7f5eacfd70c04b896c48210e0a33bb395c849e8
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2021-05-11 22:27:22 +0200

    create project skeleton closes #5

---
+++ lib/styles/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  const Colors();
+  static const Color primaryColor = const Color(0xFF151C26);
+  static const Color secondaryColor = const Color(0xFFF4C10F);
+  static const Color titleColor = const Color(0xFF5A606B);
+}
