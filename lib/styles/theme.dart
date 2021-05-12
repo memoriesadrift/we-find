3d34c442f5306eb5db3f5c4accd79dfee512d0b4
Samuel Šulovský <71935095+memoriesadrift@users.noreply.github.com>
2021-05-12 17:46:15 +0200

    Basic Text Styles and Colours, closes #9

--- lib/styles/theme.dart
+++ lib/styles/theme.dart
@@ -3,7 +3,26 @@ import 'package:flutter/material.dart';
 class Colors {
   const Colors();
   static const Color primaryColor = const Color(0xFFB80C09);
-  static const Color secondaryColor = const Color(0xFFF4C10F);
+  static const Color secondaryColor = const Color(0xFFFFAE03);
   static const Color titleColor = const Color(0xFF5A606B);
+  static const Color textColor = titleColor;
   static const Color accentColor = const Color(0xFFFFFFFC);
+}
+
+class Text {
+  const Text();
+  static const TextStyle titleStyle = const TextStyle(
+      fontWeight: FontWeight.w500, fontSize: 72, color: Colors.titleColor);
+  static const TextStyle textStyle = const TextStyle(
+      fontWeight: FontWeight.w200, fontSize: 12, color: Colors.textColor);
+  static const TextStyle buttonAccentTextStyle = const TextStyle(
+      fontWeight: FontWeight.w200, fontSize: 16, color: Colors.accentColor);
+  static const TextStyle heading1Style = const TextStyle(
+      fontWeight: FontWeight.w500, fontSize: 48, color: Colors.titleColor);
+  static const TextStyle heading2Style = const TextStyle(
+      fontWeight: FontWeight.w500, fontSize: 36, color: Colors.titleColor);
+  static const TextStyle heading3Style = const TextStyle(
+      fontWeight: FontWeight.w500, fontSize: 28, color: Colors.titleColor);
+  static const TextStyle heading4Style = const TextStyle(
+      fontWeight: FontWeight.w500, fontSize: 22, color: Colors.titleColor);
 }
