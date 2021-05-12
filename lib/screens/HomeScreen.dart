fce9e5993779ec2e855f933b9fed21c16d921e2d
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2021-05-12 18:46:36 +0200

    add null-safety

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:we_find/styles/theme.dart' as Theme;
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
