1d19e8ad4838364b4363f371348bef4fa5e102a1
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2021-05-16 11:17:52 +0200

    Wrap the root app widget into a MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:we_find/providers/fav_course_provider.dart';
+import 'package:we_find/providers/lang_provider.dart';
 import 'package:we_find/screens/HomeScreen.dart';
 import 'package:we_find/styles/theme.dart' as Theme;
 
-void main() => runApp(WeFindApp());
+void main() => runApp(appProvider(WeFindApp()));
+
+/// Wraps the supplied [appRoot] into a [MultiProvider] that gives access to
+/// all the shared states for the Widget-tree.
+MultiProvider appProvider(Widget appRoot) => MultiProvider(
+      providers: [
+        ChangeNotifierProvider<FavCourseProvider>(
+          create: (_) => FavCourseProvider(),
+        ),
+        ChangeNotifierProvider<LangProvider>(
+          create: (_) => LangProvider(),
+        )
+      ],
+      child: appRoot,
+    );
 
 class WeFindApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'we:find',
-        theme: Theme.lightThemeData,
-        home: HomeScreen());
+      title: 'we:find',
+      theme: Theme.lightThemeData,
+      home: HomeScreen(),
+    );
   }
 }
