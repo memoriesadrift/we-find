473b56aef2799e59880f6164f0a8b685f0d66e39
Péter Gyarmati <40776291+peter-gy@users.noreply.github.com>
2021-05-16 10:35:31 +0200

    I33 providers (#40)
    * override equals and hashcode for Course, create FavCourseProvider
    
    * create LangProvider

---
+++ lib/providers/lang_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:we_find/model/I18NString.dart';
+
+/// Provider that keeps track of the language set by the user
+class LangProvider with ChangeNotifier {
+  Lang _currentLang = Lang.DE;
+
+  Lang get currentLang => _currentLang;
+
+  /// Changes the language to [newLang] if it differs from the current language.
+  void changeLang(Lang newLang) {
+    // Trigger the listener notification only if a change actually occurred
+    if (_currentLang != newLang) {
+      _currentLang = newLang;
+      notifyListeners();
+    }
+  }
+}
